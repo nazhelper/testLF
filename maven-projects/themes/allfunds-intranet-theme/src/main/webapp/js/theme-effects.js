47e9b6ca7b91f67b099146ab6bf4fdb36e3557fa
ATSISTEMAS\jlflores <juanluisfloresgarcia>
2017-03-14 16:36:00 +0100

    Cambios CSS y añadido el accordion

--- maven-projects/themes/allfunds-intranet-theme/src/main/webapp/js/theme-effects.js
+++ maven-projects/themes/allfunds-intranet-theme/src/main/webapp/js/theme-effects.js
@@ -1,19 +1,27 @@
 /**
  * 
  */
-	$(function () {
-		$(window).scroll(function () {
-			if (jQuery(this).scrollTop() > 200) {
-				$('.irarriba').removeClass('oculto').fadeIn();
-				} else {
-				$('.irarriba').fadeOut()
-			}
-		});
-		
-		$('.irarriba').click(function () {
-			$('body,html').animate({
-			scrollTop: 0
-			}, 800);
-			return false;
-		});
+$(function() {
+	$(window).scroll(function() {
+		if (jQuery(this).scrollTop() > 200) {
+			$('.irarriba').removeClass('oculto').fadeIn();
+		} else {
+			$('.irarriba').fadeOut()
+		}
 	});
+
+	$('.irarriba').click(function() {
+		$('body,html').animate({
+			scrollTop : 0
+		}, 800);
+		return false;
+	});
+});
+
+$(function() {
+	var accordion = $(".panel-accordion").parent();
+	
+	accordion.accordion({
+		collapsible : true
+	});
+});
