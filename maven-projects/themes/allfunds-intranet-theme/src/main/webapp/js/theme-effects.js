a841a11a60f5fdba6c84db52ea2fbf0cbb7f94e0
nhelices <nhelices@atsistemas.com>
2017-03-15 09:47:53 +0100

    JS ADT Noticias

--- maven-projects/themes/allfunds-intranet-theme/src/main/webapp/js/theme-effects.js
+++ maven-projects/themes/allfunds-intranet-theme/src/main/webapp/js/theme-effects.js
@@ -1,6 +1,24 @@
 /**
  * 
  */
+
+var modal = document.getElementById('myModal');
+
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+    modal.style.display = "none";
+}
+
 $(function() {
 	$(window).scroll(function() {
 		if (jQuery(this).scrollTop() > 200) {
@@ -22,6 +40,8 @@ $(function() {
 	var accordion = $(".panel-accordion").parent();
 	
 	accordion.accordion({
-		collapsible : true
+		active: 1,
+		collapsible : true,
+		animate: 200
 	});
 });
