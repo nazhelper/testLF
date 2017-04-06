53cd087d4710bad61803fa777440e31dc1018e9f
ATSISTEMAS\jlflores <juanluisfloresgarcia>
2017-04-06 09:21:54 +0200

    Constants

--- maven-projects/allfunds-intranet/allfunds-intranet-portlets/allfunds-intranet-contact-portlet/src/main/java/com/allfunds/intranet/plugins/portlet/Constants.java
+++ maven-projects/allfunds-intranet/allfunds-intranet-portlets/allfunds-intranet-contact-portlet/src/main/java/com/allfunds/intranet/plugins/portlet/Constants.java
@@ -12,4 +12,7 @@ public final class Constants {
 	//Messages
 	public static final String ERROR_STRUCTURE = "Please check if the structure is correct and then try again.";
 	public static final String SUCCESS_MESSAGE = "Email Sent.";
+	public static final String ERROR_EMAILAREA = "The field Mail of the Area is Empty.";
+	public static final String ERROR_MAIL_INCORRECT = "The Format of Email is Incorrect.";
+	
 }
